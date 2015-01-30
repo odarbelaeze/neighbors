69e92f68bf5f53a55cbd249591aff9a685b2b525
Oscar David Arbeláez <odarbelaeze@gmail.com>
2015-01-30 16:15:09 -0500

    Adding the Coordinates class

---
+++ include/Coordinate.h
@@ -0,0 +1,111 @@
+#ifndef COORDINATE_H_
+#define COORDINATE_H_
+
+#include <array>
+#include <cmath>
+
+class Coordinate
+{
+    public:
+
+        /**
+         * @brief Type of value to store the coordinates
+         */
+        typedef double value_type;
+
+        /**
+         * @brief Underlying storage array
+         */
+        typedef std::array<value_type, 3> store_type;
+
+        /**
+         * @brief Builds a coordinate out of the tree coordinates
+         *
+         * @param x
+         * @param y
+         * @param z
+         */
+        Coordinate (value_type x, value_type y, value_type z);
+
+        /**
+         * @brief Builds a coordinate out of an underlying array
+         *
+         * @param array
+         */
+        Coordinate (const store_type& array);
+
+        /**
+         * @brief Forcing the compiler to generate a copy constructor
+         *
+         * @param Coordinate
+         */
+        Coordinate (const Coordinate&) = default;
+
+        /**
+         * @brief Forcing the compiler to generate a move constructor
+         *
+         * @param
+         */
+        Coordinate (Coordinate&&) = default;
+
+        /**
+         * @brief Destroys the coordinate
+         */
+        virtual ~Coordinate ();
+
+        /**
+         * @brief Bracket accesor
+         *
+         * @param i
+         * @return Coordinate at i
+         */
+        value_type operator[] (std::size_t i) const;
+
+        /**
+         * @brief Returns the coordinate at `x`
+         *
+         * @return Coordinate at `x`
+         */
+        value_type x() const;
+
+        /**
+         * @brief Returns the coordinate at `y`
+         *
+         * @return Coordinate at `y`
+         */
+        value_type y() const;
+
+        /**
+         * @brief Returns the coordinate at `y`
+         *
+         * @return Coordinate at `y`
+         */
+        value_type z() const;
+
+        /**
+         * @brief Returns the euclidean distance to other coordinate
+         *
+         * @param other
+         *
+         * @return Distance
+         */
+        value_type distanceTo(const Coordinate& other);
+
+        /**
+         * @brief Returns a new coordinate instance
+         *
+         * Computes a new Coordinate instance by moving to the coordinate other
+         * using this coordinate as the origin.
+         *
+         * @param other
+         *
+         * @return A new coordinate instance
+         */
+        Coordinate move(const Coordinate& other);
+
+    protected:
+        store_type _vec;
+
+};
+
+#endif
---
+++ src/Coordinate.cc
@@ -0,0 +1,65 @@
+#include "Coordinate.h"
+
+
+Coordinate::Coordinate (value_type x, value_type y, value_type z)
+    : _vec{ {x, y, z} }
+{
+}
+
+
+Coordinate::Coordinate (const store_type& array)
+    : _vec(array)
+{
+}
+
+
+Coordinate::~Coordinate ()
+{
+}
+
+
+Coordinate::value_type Coordinate::operator[] (std::size_t i) const
+{
+    return _vec[i];
+}
+
+
+Coordinate::value_type Coordinate::x() const
+{
+    return _vec[0];
+}
+
+
+Coordinate::value_type Coordinate::y() const
+{
+    return _vec[1];
+}
+
+
+Coordinate::value_type Coordinate::z() const
+{
+    return _vec[2];
+}
+
+
+Coordinate::value_type Coordinate::distanceTo(const Coordinate& other)
+{
+    auto&& a = this->_vec;
+    auto&& b = other._vec;
+    return std::pow(
+            (b[0] - a[0]) * (b[0] - a[0]) +
+            (b[1] - a[1]) * (b[1] - a[1]) +
+            (b[2] - a[2]) * (b[2] - a[2]),
+            0.5);
+}
+
+
+Coordinate Coordinate::move(const Coordinate& other)
+{
+    Coordinate coordinate = *this;
+    coordinate._vec[0] += other._vec[0];
+    coordinate._vec[1] += other._vec[1];
+    coordinate._vec[2] += other._vec[2];
+    return coordinate;
+}
+
