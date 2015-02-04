ef67922c90d0acf83e2b385ed4ffebbb780f2a9e
Oscar David Arbeláez <odarbelaeze@gmail.com>
2015-02-04 15:23:10 -0500

    Fixing the interaction cut

--- src/neighbors.cc
+++ src/neighbors.cc
@@ -93,7 +93,7 @@ int main(int argc, char *argv[])
             if (interact)
             {
                 auto interaction = interactions.at(key);
-                if (node.distanceTo(other) < interaction.cut) {
+                if (node.distanceTo(other) <= interaction.cut) {
                     logger.log(interaction_id++, node.id(), other.id(), interaction.exchange);
                 }
             }
